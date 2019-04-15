d751e93ed8c6f68fcd9b0163fcab9d8d569a1582
Alain Trouvé <trouve@cmla.ens-cachan.fr>
2019-04-15 13:12:16 +0200

    adding a backup on exp_ipam2 after optimisation via pickle

--- script/Leaf/exp_ipam2.py
+++ script/Leaf/exp_ipam2.py
@@ -28,20 +28,22 @@ import src.ExpIpam.utils as ipam
 
 
 
-# %%
+# %%  Basi experiments
+flag = 'basi'
+(source, target) = ('basi1b.pkl', 'basi1t.pkl')
+(height_source, height_target, Dx, Dy) = (38., 100., 0., 0.)
 dir_res = '/home/trouve/Dropbox/Talks/Pics/ResImp/ResIpam/'
 dir_Mysh = '/home/trouve/Dropbox/Talks/2019_04_IPAM/tex/Mysh/'
 
 #%%
 # parametric
-flag = 'basi'
-(source, target) = ('basi1b.pkl', 'basi1t.pkl')
-(height_source, height_target, Dx, Dy) = (38., 100., 0., 0.)
+
+name_exp = 'leaf_ba2_p'
 coeffs = [0.01, 100, 0.001]
 nu = 0.001
 (sig00, sig0, sig1) = (1000., 15., 30.)
 (lam_var, sig_var) = (10., 30.)
-name_exp = 'trial'
+attach_var = (lam_var, sig_var)
 
 (P0, outvar) = ipam.exp_ipam_init(flag = flag, 
            source = source, target = target, name_exp = name_exp, 
@@ -49,297 +51,49 @@ name_exp = 'trial'
            Dx = Dx, Dy = Dy, height_source = height_source,
            height_target = height_target, sig00 = sig00, sig0 = sig0, sig1 = sig1, 
            coeffs = coeffs, nu = nu, lam_var = lam_var, sig_var = sig_var)
+          # outvar = (Module, xs, xst, opti.fun, opti.jac)
           
-# outvar = (Module, xs, xst, opti.fun, P0, opti.jac)
-#%%          
-attach_var = (lam_var, sig_var)
-P1 = ipam.exp_ipam_optim((P0, outvar), attach_var, maxiter = 1, maxfun = 200, N=10)
 
-#%%
-P0 = P1.copy()
-P1 = ipam.exp_ipam_optim((P0, outvar), attach_var, maxiter = 1, maxfun = 200, N=10)
-#%%
+maxiter = 30
+max_fun = 100
+N = 10
+P1 = ipam.exp_ipam_optim((P0, outvar), 
+                         attach_var, maxiter = maxiter, maxfun = 200, N=N)
+
+# Save the result in case
+filepkl = dir_res + name_exp + ".pkl"
+with open(filepkl, 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([P0, P1, outvar, attach_var, maxiter, max_fun, N], f)
+
+# plottings and savings
 N = 15
 invar = (N, flag, name_exp, height_source, height_target, Dx, Dy)
 (Module, xs, xst, fun, jac) = outvar
 outvarplot = (Module, P1, xs, xst, dir_res, dir_Mysh)
 ipam.exp_ipam_plot(invar, outvarplot)
-
-    
-    
 #%%
-           
-          
-def my_exp(source = 'basi1b.pkl', target = 'basi1t.pkl', name_exp = 'leaf1',
-           dir_res = '/home/trouve/Dropbox/Talks/Pics/ResImp/ResIpam/',  
-           dir_Mysh = '/home/trouve/Dropbox/Talks/2019_04_IPAM/tex/Mysh/',
-           Dx = 0., Dy = 0., height_source = 38.,
-           height_target = 100., sig00 = 200, sig0 = 15, flag = 'basi', sig1 = 30, 
-           coeffs =[0.01, 0.1, 0.01], nu = 0.001, lam_var = 40., sig_var = 30.,
-                   N=10, maxiter = 100.):
-    
-    with open('./data/' + source, 'rb') as f:
-        _, lx = pickle.load(f)
-    
-    nlx = np.asarray(lx).astype(np.float32)
-    mean_nlx = np.mean(nlx[:, 1])
-    (lmin, lmax) = (np.min(nlx[:, 1]), np.max(nlx[:, 1]))
-    scale = height_source / (lmax - lmin)
-    
-    nlx[:, 1] = Dy-scale * (nlx[:, 1] - lmax)
-    nlx[:, 0] = Dx+scale * (nlx[:, 0] - np.mean(nlx[:, 0]))
-    
-    #  target
-    with open('./data/' + target, 'rb') as f:
-        _, lxt = pickle.load(f)
-    
-    nlxt = np.asarray(lxt).astype(np.float32)
-    (lmin, lmax) = (np.min(nlxt[:, 1]), np.max(nlxt[:, 1]))
-    scale = height_target / (lmax - lmin)
-    
-    nlxt[:, 1] = - scale * (nlxt[:, 1] - lmax) 
-    nlxt[:, 0] = scale * (nlxt[:, 0] - np.mean(nlxt[:, 0])) 
-    
-    xst = nlxt[nlxt[:, 2] == 2, 0:2]
-    
-    #  common options
-    # nu = 0.001
-    dim = 2
-    
-    #  Silent Module
-    xs = nlx[nlx[:, 2] == 2, 0:2]
-    xs = np.delete(xs, 3, axis=0)
-    Sil = defmodsil.SilentLandmark(xs.shape[0], dim)
-    ps = np.zeros(xs.shape)
-    param_sil = (xs, ps)
-    
-    #  Modules of Order 0
-    # sig00 = 200
-    x00 = np.array([[0., Dy + height_source/2]])
-    Model00 = defmod0.ElasticOrder0(sig00, x00.shape[0], dim, coeffs[0], nu)
-    p00 = np.zeros([1, 2])
-    param_00 = (x00, p00)
-    
-    
-    #my_plot(x00, "Module order 00", '+r')
-    
-    #  Modules of Order 0
-    # sig0 = 15
-    x0 = nlx[nlx[:, 2] == 1, 0:2]
-    Model0 = defmod0.ElasticOrder0(sig0, x0.shape[0], dim, coeffs[1], nu)
-    p0 = np.zeros(x0.shape)
-    param_0 = (x0, p0)
-    
-    #my_plot(x0, "Module order 0", 'or')
-    
-    #  Modules of Order 1
-    # sig1 = 30
-    
-    if (flag == 'basi'):
-        x1 = nlx[nlx[:, 2] == 1, 0:2]
-        C = np.zeros((x1.shape[0], 2, 1))
-        K, L = 10, height_source
-        a, b = -2 / L ** 3, 3 / L ** 2
-        C[:, 1, 0] = (K * (a * (L - x1[:, 1]+Dy) ** 3 
-             + b * (L - x1[:, 1]+Dy) ** 2))
-        C[:, 0, 0] = 1. * C[:, 1, 0]
-    elif (flag == 'acro'):
-        x1 = nlx[nlx[:, 2] == 1, 0:2]
-        C = np.zeros((x1.shape[0], 2, 1))
-        K, L = 10, height_source
-        a,b = 1/L, 3.
-        z = a*(x1[:,1]-Dy)
-        C[:,1,0] = K*((1-b)*z**2+b*z)
-        C[:,0,0] = 0.8*C[:,1,0]
-    elif (flag == 'diffuse'):
-        x1 = nlx[nlx[:, 2] == 1, 0:2]
-        C = np.zeros((x1.shape[0], 2, 1))
-        K = 10
-        C[:,1,0] = K
-        C[:,0,0] = K
-        
-    
-    Model1 = defmod1.ElasticOrder1(sig1, x1.shape[0], dim, coeffs[2], C, nu)
-    
-    th = 0 * np.pi * np.ones(x1.shape[0])
-    R = np.asarray([rot.my_R(cth) for cth in th])
-    
-    (p1, PR) = (np.zeros(x1.shape), np.zeros((x1.shape[0], 2, 2)))
-    param_1 = ((x1, R), (p1, PR))
-    
-    plt.figure()
-    xs_c = my_close(xs)
-    plt.plot(xs_c[:, 0], xs_c[:, 1], '-g', linewidth=2)
-    plt.plot(xs[:, 0], xs[:, 1], 'og', linewidth=2)
-    plt.plot(x00[:, 0], x00[:, 1], 'ok', linewidth=2)
-    
-    plt.plot(x1[:, 0], x1[:, 1], 'ob', linewidth=2, markersize = 8)
-    plt.plot(x0[:, 0], x0[:, 1], 'or', linewidth=2, markersize = 3)
-    plt.axis('equal')
-    plt.axis('off')
-    plt.show()
-    plt.savefig(dir_res + name_exp + '_modules_' + flag + '.png', 
-                    format='png', bbox_inches='tight')
-    #my_plot(x1, "Module order 1", 'og')
-    
-    
-    #  Full model
-    
-    Module = comb_mod.CompoundModules([Sil, Model00, Model0, Model1])
-    Module.GD.fill_cot_from_param([param_sil, param_00, param_0, param_1])
-    P0 = opti.fill_Vector_from_GD(Module.GD)
-    
-    # 
-    #lam_var = 40.
-    #sig_var = 30.
-    #N = 10
-    args = (Module, xst, lam_var, sig_var, N, 1e-7)
-    
-    res = scipy.optimize.minimize(opti.fun, P0,
-                                  args=args,
-                                  method='L-BFGS-B',
-                                  jac=opti.jac,
-                                  bounds=None,
-                                  tol=None,
-                                  callback=None,
-                                  options={
-                                      'maxcor': 10,
-                                      'ftol': 1.e-09,
-                                      'gtol': 1e-03,
-                                      'eps': 1e-08,
-                                      'maxfun': 200,
-                                      'maxiter': maxiter,
-                                      'iprint': 1,
-                                      'maxls': 25
-                                  })
-    
-    P1 = res['x']
-    opti.fill_Mod_from_Vector(P1, Module)
-    Module_optimized = Module.copy_full()
-    Modules_list = shoot.shooting_traj(Module, N)
-    
-    # Show data
-    
-    fig = plt.figure(figsize=(8, 6)) 
-    gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1]) 
-    plt.figure()
-    plt.subplot(gs[0])
-    if (flag == 'basi'):
-        img = mpimg.imread('./data/Model1.png')
-    elif (flag == 'acro'):
-        img = mpimg.imread('./data/Model2.png')
-    elif (flag == 'diffuse'):
-        img = mpimg.imread('./data/Model3.png')
-        
-    imgplot = plt.imshow(img)
-    plt.axis('off')
-    plt.subplot(gs[1])
-    z = np.linspace(0,1,100)
-    if (flag == 'basi'):
-        ax=plt.plot(-2*(1-z)**3+3*(1-z)**2,z)
-        axes = plt.gca()
-        axes.set_xlim([0.,1.])
-        axes.set_ylim([0.,1.])
-        plt.axis('tight')
-    elif (flag == 'acro'):
-        img = mpimg.imread('./data/Model2.png')
-        ax=plt.plot((1-3)*z**2+3*z,z)
-        plt.axis('tight')
-    elif (flag == 'diffuse'):
-        img = mpimg.imread('./data/Model3.png')
-        plt.plot(np.ones(z.shape),z)
-        plt.axis('tight')
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(dir_res + name_exp + '_model_' + flag + '.png', 
-                    format='png', bbox_inches='tight')
-    
-    #  Visualisation
-    xst_c = my_close(xst)
-    xs_c = my_close(xs)
-    for i in range(N + 1):
-        plt.figure()
-        xs_i = Modules_list[2 * i].GD.GD_list[0].GD
-        xs_ic = my_close(xs_i)
-        plt.plot(xs_ic[:, 0], xs_ic[:, 1], '-g', linewidth=2)
-        
-        x00_i = Modules_list[2 * i].GD.GD_list[2].GD
-        plt.plot(x00_i[:, 0], x00_i[:, 1], 'ok', linewidth=2)
-        
-        plt.plot(xst_c[:, 0], xst_c[:, 1], '-k', linewidth=2)
-        plt.plot(xs_c[:, 0], xs_c[:, 1], '-b', linewidth=2)
-        
-        x0_i = Modules_list[2 * i].GD.GD_list[1].GD
-        plt.plot(x0_i[:, 0], x0_i[:, 1], 'or', linewidth=2, markersize = 3)
-        plt.axis('equal')
-        plt.show()
-    
-    #  With grid
-    hxgrid = 9
-    hsl = 1.2*height_source/2
-    a, b, c, d = (Dx-hsl/2, Dx+hsl/2, Dy, Dy+2*hsl) 
-    hygrid = np.round(hxgrid*(d-c)/(b-a))
-    nxgrid, nygrid = (2*hxgrid+1, 2*hygrid+1) # create a grid for visualisation purpose
-    
-    [xx, xy] = np.meshgrid(np.linspace(a, b, nxgrid), np.linspace(c, d, nygrid))
-    (nxgrid, nygrid) = xx.shape
-    grid_points = np.asarray([xx.flatten(), xy.flatten()]).transpose()
-    
-    Sil_grid = defmodsil.SilentLandmark(grid_points.shape[0], dim)
-    
-    param_grid = (grid_points, np.zeros(grid_points.shape))
-    Sil_grid.GD.fill_cot_from_param(param_grid)
-    
-    Mod_tot = comb_mod.CompoundModules([Sil_grid, Module_optimized.copy_full()])
-    
-    # 
-    N = 15
-    Modlist_opti_tot_grid = shoot.shooting_traj(Mod_tot, N)
-    #  Plot with grid
-    xs_c = my_close(xs)
-    xst_c = my_close(xst)
-    for i in range(N + 1):
-        plt.figure()
-        xgrid = Modlist_opti_tot_grid[2 * i].GD.GD_list[0].GD
-        xsx = xgrid[:, 0].reshape((nxgrid, nygrid))
-        xsy = xgrid[:, 1].reshape((nxgrid, nygrid))
-        plt.plot(xsx, xsy, color='lightblue')
-        plt.plot(xsx.transpose(), xsy.transpose(), color='lightblue')
-        xs_i = Modlist_opti_tot_grid[2 * i].GD.GD_list[1].GD_list[0].GD
-        xs_ic = my_close(xs_i)
-        # plt.plot(xs[:,0], xs[:,1], '-b', linewidth=1)
-        plt.plot(xst_c[:, 0], xst_c[:, 1], '-k', linewidth=2)
-        plt.plot(xs_ic[:, 0], xs_ic[:, 1], '-g', linewidth=2)
-        plt.plot(xs_c[:, 0], xs_c[:, 1], '-b', linewidth=2)
-        plt.axis('equal')
-        # plt.axis([-10,10,-10,55])
-        
-        plt.axis( [- 1.1*height_target/2, + 1.1*height_target/2, 
-                   - 0.05*height_target, + 1.05*height_target])
-        # plt.axis('off')
-        #plt.show()
-        plt.savefig(dir_res + name_exp + '_t_' + '{:02d}'.format(i) + '.png', 
-                    format='png', bbox_inches='tight',dpi=300)
-        
-    filepng = dir_res + name_exp + '_t_' + "*.png"    
-    filegif = dir_res + name_exp + ".gif"
-    filegifr = dir_res + name_exp + "r.gif"
-     
-    #os.system("convert " + filepng + " -coalesce -duplicate 1,-2--1" +
-    #          "-quiet -layers OptimizePlus  -loop 0 " + filegif) 
 
-    os.system("convert " + filepng + " -reverse -set delay 0 " + filegifr)  
-    os.system("convert " + filepng + " -set delay 0 " 
-               + filegifr + "  -loop 0 " + filegif)  
+from_past = True
+if (from_past):
+    with open(filepkl, 'rb') as f:  # Python 3: open(..., 'rb')
+     P0, P1, outvar, attach_var, maxiter, max_fun, N = pickle.load(f)
+else:
+    P0 = P1.copy()
+         
+P1 = ipam.exp_ipam_optim((P0, outvar), attach_var, maxiter = 1, maxfun = 200, N=10)
+
+# Save  again the result in case
+filepkl = dir_res + name_exp + ".pkl"
+with open(filepkl, 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([P0, P1, outvar, attach_var, maxiter, max_fun, N], f)
     
-    # create the mysh file for inclusion in the beamer latex
-    filemysh = dir_Mysh + name_exp + ".mysh"
-    file = open(filemysh,"w")
-    file.write("#!/bin/bash -x\n\n")
-    file.write("gwenview " + filegif + " &")
-    file.close()           
-               
-    
+# plottings and savings
+N = 15
+invar = (N, flag, name_exp, height_source, height_target, Dx, Dy)
+(Module, xs, xst, fun, jac) = outvar
+outvarplot = (Module, P1, xs, xst, dir_res, dir_Mysh)
+ipam.exp_ipam_plot(invar, outvarplot)
+ 
 #%%
 my_exp(source = 'basi1b.pkl', target = 'basi1t.pkl', name_exp = 'leafaa', 
        Dx = 0., Dy = 0., height_source = 38.,
